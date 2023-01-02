f76824b447be670e06dcf7aae3a5a7177b4d8b12
blackout3d <121775934+blackout3d@users.noreply.github.com>
2023-01-01 17:04:55 -0800

    Delete resources directory

--- resources/scripts/script.js
+++
@@ -1,9 +0,0 @@
-/*
-    By: Tyger Whitaker
-    File Name: script.js
-*/
-
-//jQuery for hero image to consume the header window space
-$(document).ready(function(){
-    $('.hero').height($(window).height());
-});
